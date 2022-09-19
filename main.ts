ebdae0c37b7dd59a40fb96499d3fa43493a0f8c6
Marcelo Fernandes <37639856+tchelomgf@users.noreply.github.com>
2022-09-19 19:19:20 -0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 input.onButtonPressed(Button.B, function () {
-    max = 0
+    peak = 0
+    maxlevel = 0
     pins.setPull(DigitalPin.P2, PinPullMode.PullNone)
     strip.clear()
     pins.digitalWritePin(DigitalPin.P2, 0)
@@ -7,8 +8,9 @@ input.onButtonPressed(Button.B, function () {
 })
 let num = 0
 let mic = 0
-let micold = 0
-let max = 0
+let peak = 0
+let level = 0
+let maxlevel = 0
 let strip: neopixel.Strip = null
 let temp = 0
 pins.setPull(DigitalPin.P2, PinPullMode.PullNone)
@@ -18,25 +20,23 @@ strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
 strip.setBrightness(50)
 strip.show()
 loops.everyInterval(1000, function () {
-    if (max > 0) {
-        max = max - 1
+    if (peak > 0) {
+        peak = peak - 1
     }
 })
 basic.forever(function () {
+    // led.plotBarGraph(pins.analogReadPin(AnalogPin.P1) - offset,400)
+    level = input.soundLevel()
+    if (level > maxlevel) { maxlevel = level}
     led.plotBarGraph(
-    pins.analogReadPin(AnalogPin.P1),
-    400
+    level,
+    maxlevel
     )
 })
 basic.forever(function () {
     pins.analogSetPitchPin(AnalogPin.P1)
-    pins.analogPitch(1000, 0)
-    micold = mic
-    mic = Math.map(pins.analogReadPin(AnalogPin.P1), 0, 128, 0, 24)
-    // filter
-    if (micold < 2 && mic > 12 && mic < 14) {
-        mic = 0
-    }
+    //mic = Math.map(pins.analogReadPin(AnalogPin.P1) - offset, 0, 480, 0, 24)
+    mic = Math.map(input.soundLevel(), 0, maxlevel, 0, 24)
     strip.clear()
     while (num < mic && num < 23) {
         strip.setPixelColor(num, neopixel.colors(NeoPixelColors.Blue))
@@ -44,13 +44,13 @@ basic.forever(function () {
         strip.show()
         num = num + 1
     }
-    if (max < num) {
-        max = num
-        if (max > 23) {
-            max = 23
+    if (peak < num) {
+        peak = num
+        if (peak > 23) {
+            peak = 23
         }
     }
-    strip.setPixelColor(max, neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(peak, neopixel.colors(NeoPixelColors.Red))
     strip.show()
     num = 0
     basic.pause(100)
