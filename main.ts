16266dd589d5fc085c95109e02a2bc2245072699
Marcelo Fernandes <37639856+tchelomgf@users.noreply.github.com>
2022-09-19 17:43:15 -0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,36 +1,51 @@
 input.onButtonPressed(Button.B, function () {
+    max = 0
     pins.setPull(DigitalPin.P2, PinPullMode.PullNone)
     strip.clear()
     pins.digitalWritePin(DigitalPin.P2, 0)
     strip.show()
 })
+let num = 0
+let mic = 0
+let max = 0
 let strip: neopixel.Strip = null
+let temp = 0
 pins.setPull(DigitalPin.P2, PinPullMode.PullNone)
 pins.setPull(DigitalPin.P1, PinPullMode.PullNone)
-let temp = 0
-let mic = 0
-let num = 0
 strip = neopixel.create(DigitalPin.P2, 24, NeoPixelMode.RGB)
 strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
 strip.setBrightness(50)
 strip.show()
+loops.everyInterval(1000, function () {
+    if (max > 0) {
+        max = max - 1
+    }
+})
+basic.forever(function () {
+    led.plotBarGraph(
+    pins.analogReadPin(AnalogPin.P1),
+    400
+    )
+})
 basic.forever(function () {
     pins.analogSetPitchPin(AnalogPin.P1)
     pins.analogPitch(1000, 0)
-    mic = Math.map(pins.analogReadPin(AnalogPin.P1), 0, 30, 0, 24)
-    while (num < mic) {
+    mic = Math.map(pins.analogReadPin(AnalogPin.P1), 0, 128, 0, 24)
+    strip.clear()
+    while (num < mic && num < 23) {
         strip.setPixelColor(num, neopixel.colors(NeoPixelColors.Blue))
         strip.setBrightness(50)
         strip.show()
         num = num + 1
     }
+    if (max < num) {
+        max = num
+        if (max > 23) {
+            max = 23
+        }
+    }
+    strip.setPixelColor(max, neopixel.colors(NeoPixelColors.Red))
+    strip.show()
     num = 0
     basic.pause(100)
-    strip.clear()
-})
-basic.forever(function () {
-    led.plotBarGraph(
-    pins.analogReadPin(AnalogPin.P1),
-    1023
-    )
 })
