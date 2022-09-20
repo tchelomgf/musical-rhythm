fb9b3abe7fe319de2bbdb26692b857e13474ac04
Marcelo Fernandes <37639856+tchelomgf@users.noreply.github.com>
2022-09-20 09:26:10 -0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,13 +7,14 @@ input.onButtonPressed(Button.B, function () {
     pins.digitalWritePin(DigitalPin.P2, 0)
     strip.show()
 })
+let peakcount = 0
 let num = 0
 let mic = 0
-let peak = 0
-let level = 0
-let maxlevel = 0
 let minlevel = 0
+let level = 0
 let run = false
+let maxlevel = 0
+let peak = 0
 let strip: neopixel.Strip = null
 let temp = 0
 pins.setPull(DigitalPin.P2, PinPullMode.PullNone)
@@ -22,34 +23,18 @@ strip = neopixel.create(DigitalPin.P2, 24, NeoPixelMode.RGB)
 strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
 strip.setBrightness(50)
 strip.show()
-loops.everyInterval(1000, function () {
-    if (run) {
-        if (peak > 0) {
-            peak = peak - 1
-        }
-    }
-    else {
-        minlevel = input.soundLevel()
-        maxlevel = minlevel + 1 
-        run = true
-    }    
-})
 basic.forever(function () {
-    //pins.analogSetPitchPin(AnalogPin.P1)
-    //mic = Math.map(pins.analogReadPin(AnalogPin.P1) - offset, 0, 480, 0, 24)
-
     if (run) {
-        // led.plotBarGraph(pins.analogReadPin(AnalogPin.P1) - offset,400)
         level = input.soundLevel()
-
         if (maxlevel < level) {
             maxlevel = level
         } else if (minlevel > level) {
             minlevel = level
         }
-
-        led.plotBarGraph(level,maxlevel)
-
+        led.plotBarGraph(
+        level,
+        maxlevel
+        )
         mic = Math.map(level, minlevel, maxlevel, 0, 24)
         strip.clear()
         while (num < mic && num < 23) {
@@ -60,6 +45,7 @@ basic.forever(function () {
         }
         if (peak < num) {
             peak = num
+            peakcount = 20
             if (peak > 23) {
                 peak = 23
             }
@@ -69,4 +55,19 @@ basic.forever(function () {
         num = 0
     }
     basic.pause(100)
+})
+loops.everyInterval(100, function () {
+    if (run) {
+        if (peakcount > 0) {
+            peakcount = peakcount - 1
+        } else {
+            if (peak > 0) {
+                peak = peak - 1
+            }
+        }
+    } else {
+        minlevel = input.soundLevel()
+        maxlevel = minlevel + 1
+        run = true
+    }
 })
