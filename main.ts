314dc56a8d91a79bda8db9780687d15e276b37c9
Marcelo Fernandes <37639856+tchelomgf@users.noreply.github.com>
2022-09-20 08:19:11 -0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 input.onButtonPressed(Button.B, function () {
     peak = 0
     maxlevel = 0
+    run = false
     pins.setPull(DigitalPin.P2, PinPullMode.PullNone)
     strip.clear()
     pins.digitalWritePin(DigitalPin.P2, 0)
@@ -11,6 +12,8 @@ let mic = 0
 let peak = 0
 let level = 0
 let maxlevel = 0
+let minlevel = 0
+let run = false
 let strip: neopixel.Strip = null
 let temp = 0
 pins.setPull(DigitalPin.P2, PinPullMode.PullNone)
@@ -20,38 +23,50 @@ strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
 strip.setBrightness(50)
 strip.show()
 loops.everyInterval(1000, function () {
-    if (peak > 0) {
-        peak = peak - 1
-    }
-})
-basic.forever(function () {
-    // led.plotBarGraph(pins.analogReadPin(AnalogPin.P1) - offset,400)
-    level = input.soundLevel()
-    if (level > maxlevel) { maxlevel = level}
-    led.plotBarGraph(
-    level,
-    maxlevel
-    )
-})
-basic.forever(function () {
-    pins.analogSetPitchPin(AnalogPin.P1)
-    //mic = Math.map(pins.analogReadPin(AnalogPin.P1) - offset, 0, 480, 0, 24)
-    mic = Math.map(input.soundLevel(), 0, maxlevel, 0, 24)
-    strip.clear()
-    while (num < mic && num < 23) {
-        strip.setPixelColor(num, neopixel.colors(NeoPixelColors.Blue))
-        strip.setBrightness(50)
-        strip.show()
-        num = num + 1
-    }
-    if (peak < num) {
-        peak = num
-        if (peak > 23) {
-            peak = 23
+    if (run) {
+        if (peak > 0) {
+            peak = peak - 1
         }
     }
-    strip.setPixelColor(peak, neopixel.colors(NeoPixelColors.Red))
-    strip.show()
-    num = 0
+    else {
+        minlevel = input.soundLevel()
+        maxlevel = minlevel + 1 
+        run = true
+    }    
+})
+basic.forever(function () {
+    //pins.analogSetPitchPin(AnalogPin.P1)
+    //mic = Math.map(pins.analogReadPin(AnalogPin.P1) - offset, 0, 480, 0, 24)
+
+    if (run) {
+        // led.plotBarGraph(pins.analogReadPin(AnalogPin.P1) - offset,400)
+        level = input.soundLevel()
+
+        if (maxlevel < level) {
+            maxlevel = level
+        } else if (minlevel > level) {
+            minlevel = level
+        }
+
+        led.plotBarGraph(level,maxlevel)
+
+        mic = Math.map(level, minlevel, maxlevel, 0, 24)
+        strip.clear()
+        while (num < mic && num < 23) {
+            strip.setPixelColor(num, neopixel.colors(NeoPixelColors.Blue))
+            strip.setBrightness(50)
+            strip.show()
+            num = num + 1
+        }
+        if (peak < num) {
+            peak = num
+            if (peak > 23) {
+                peak = 23
+            }
+        }
+        strip.setPixelColor(peak, neopixel.colors(NeoPixelColors.Red))
+        strip.show()
+        num = 0
+    }
     basic.pause(100)
 })
